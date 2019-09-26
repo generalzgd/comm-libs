b5aa026f961760ef434daded2e6de748311095e3
zhangguodong <zhangguodong@bianfeng.com>
2019-09-26 19:23:06 +0800

    add test for performance compair

--- pickjson/pick_test.go
+++ pickjson/pick_test.go
@@ -11,9 +11,24 @@
 package pickjson
 
 import (
-	"reflect"
+	`encoding/json`
+	`reflect`
 	"testing"
+	`time`
 )
+
+func TestStandLibJson(t *testing.T)  {
+	parseJson := func(src []byte, field string) interface{} {
+		out := map[string]interface{}{}
+		json.Unmarshal(src, &out)
+		return out[field]
+	}
+	begin := time.Now()
+	for i:=0; i<100000; i++{
+		parseJson([]byte(`{"foo":1,"a":"str","b":0.25}`), "b")
+	}
+	t.Logf("use time:%v", time.Since(begin)) // use time:810.0463ms
+}
 
 func TestPickBytes(t *testing.T) {
 	type args struct {
@@ -32,7 +47,7 @@ func TestPickBytes(t *testing.T) {
 				source: []byte(`{"foo":1,"a":"str","b":0.25}`),
 				field:  []byte("b"),
 			},
-			want: []byte("0.25"),
+			want: []byte("0.25"), // use time:174.0099ms
 		},
 		{
 			name: "TestPickBytes_2",
@@ -40,7 +55,7 @@ func TestPickBytes(t *testing.T) {
 				source: []byte(`{"foo":1,"a":"str","b":0.25,"d":{"t":234}}`),
 				field:  []byte("d"),
 			},
-			want: []byte(`{"t":234}`),
+			want: []byte(`{"t":234}`), // use time:334.0191ms
 		},
 		{
 			name: "TestPickBytes_3",
@@ -48,14 +63,18 @@ func TestPickBytes(t *testing.T) {
 				source: []byte(`{"foo":1,"a":"str","b":0.25,"d":{"t":234, "a":222}}`),
 				field:  []byte("a"),
 			},
-			want: []byte(`222`),
+			want: []byte(`"str"`), // use time:168.0096ms
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := PickBytes(tt.args.source, tt.args.field); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("PickBytes() = %v, want %v", got, tt.want)
+			begin := time.Now()
+			for i:=0;i<100000;i++ {
+				if got := PickBytes(tt.args.source, tt.args.field); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("PickBytes() = %v, want %v", got, tt.want)
+				}
 			}
+			t.Logf("use time:%v", time.Since(begin))
 		})
 	}
 }
