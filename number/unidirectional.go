2c265635958fd2fb477d08e0266a77bf93f66333
zgd <zhangguodong@bianfeng.com>
2020-01-03 16:00:42 +0800

    add addition handler for unidirection number

--- number/unidirectional.go
+++ number/unidirectional.go
@@ -17,6 +17,9 @@ import (
 	`sync`
 )
 
+// 额外的方法处理，保证前后调用的一致性
+type AdditionalHandler func(uint64)
+
 // 单向增加数字
 type UnidirectionalNum struct {
 	lock sync.RWMutex
@@ -31,20 +34,27 @@ func (p *UnidirectionalNum) GetNumber() uint64 {
 }
 
 // 如果比自己小，则忽略。当且仅当maxuint64时，才允许设置小自己的值
-func (p *UnidirectionalNum) SetNumber(v uint64) {
+func (p *UnidirectionalNum) SetNumber(v uint64, fs ...AdditionalHandler) {
 	p.lock.Lock()
 	defer p.lock.Unlock()
 
 	if p.num == math.MaxUint64 || p.num < v {
 		p.num = v
+
+		for _, f := range fs {
+			f(p.num)
+		}
 	}
 }
 
 // 超过max.Uint64后，从0开始
-func (p *UnidirectionalNum) AutoIncrease() uint64 {
+func (p *UnidirectionalNum) AutoIncrease(fs ...AdditionalHandler) uint64 {
 	p.lock.Lock()
 	defer p.lock.Unlock()
 
 	p.num += 1
+	for _, f := range fs {
+		f(p.num)
+	}
 	return p.num
 }
