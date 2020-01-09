f5b2eb75223042f5cd8c19d5ad1cc2c841a524a8
zgd <zhangguodong@bianfeng.com>
2020-01-09 16:14:24 +0800

    add get inner ip method

--- iputil.go
+++ iputil.go
@@ -12,7 +12,9 @@ package comm_libs
 
 import (
 	`fmt`
+	`io/ioutil`
 	`net`
+	`net/http`
 	`strings`
 )
 
@@ -44,4 +46,64 @@ func Long2Ip(v uint32) string {
 
 	res := fmt.Sprintf("%d.%d.%d.%d", v1, v2, v3, v4)
 	return res
+}
+
+
+func GetInnerIp() string {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return ""
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0{
+			continue
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+		if strings.HasPrefix(iface.Name, "docker") || strings.HasPrefix(iface.Name, "w-") ||
+			strings.HasPrefix(iface.Name, "isatap") {
+			continue
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return ""
+		}
+		// fmt.Println(iface.Name, addrs)
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue
+			}
+			// if ip[0] == 10 || ip[0] == 172 || (ip[0]==192 && ip[1] == 168) {
+			// 	return ip.String()
+			// }
+			return ip.String()
+		}
+	}
+	return ""
+}
+
+func GetExternIp() string {
+	resp, err := http.Get("http://myexternalip.com/raw")
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+	return strings.TrimSpace(string(bs)) // IpStr2Ip(string(bs))
 }
